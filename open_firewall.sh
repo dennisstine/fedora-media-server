92ff9ee28989ad1adfd15ea736ebebf9a73ec6a9
Dennis Stine <dennis.stine@gmail.com>
2023-12-22 15:01:56 -0600

    Switched firewall zone
    Adding to "public" with fedora did not work.  Added to the default FedoraServer zone instead.

--- open_firewall.sh
+++ open_firewall.sh
@@ -2,30 +2,30 @@
 
 sudo firewall-cmd --list-all
 
-sudo firewall-cmd --zone=public --permanent --add-service=http
-sudo firewall-cmd --zone=public --permanent --add-service=https
+sudo firewall-cmd --zone=FedoraServer --permanent --add-service=http
+sudo firewall-cmd --zone=FedoraServer --permanent --add-service=https
 
 # docker tcp
-sudo firewall-cmd --zone=public --permanent --add-port 2375/tcp
+sudo firewall-cmd --zone=FedoraServer --permanent --add-port 2375/tcp
 
 # plex ui port - others can be opened as well
-sudo firewall-cmd --zone=public --permanent --add-port 32400/tcp
+sudo firewall-cmd --zone=FedoraServer --permanent --add-port 32400/tcp
 
 # sabnzbd
-sudo firewall-cmd --zone=public --permanent --add-port 8080/tcp
-sudo firewall-cmd --zone=public --permanent --add-port 9090/tcp
+sudo firewall-cmd --zone=FedoraServer --permanent --add-port 8080/tcp
+sudo firewall-cmd --zone=FedoraServer --permanent --add-port 9090/tcp
 
 # sonarr
-sudo firewall-cmd --zone=public --permanent --add-port 8989/tcp
+sudo firewall-cmd --zone=FedoraServer --permanent --add-port 8989/tcp
 
 # radarr
-sudo firewall-cmd --zone=public --permanent --add-port 7878/tcp
+sudo firewall-cmd --zone=FedoraServer --permanent --add-port 7878/tcp
 
 # tautulli
-sudo firewall-cmd --zone=public --permanent --add-port 8181/tcp
+sudo firewall-cmd --zone=FedoraServer --permanent --add-port 8181/tcp
 
 # cadvisor
-sudo firewall-cmd --zone=public --permanent --add-port 8282/tcp
+sudo firewall-cmd --zone=FedoraServer --permanent --add-port 8282/tcp
 
 
 sudo firewall-cmd --reload
